c54e139f9916c80ce9f44ad85a34ba894cf5202b
Matt Murphy <caevyn@gmail.com>
2013-04-20 11:55:09 +1000

    rename package to just lua to make cinst easier

--- src/tools/ChocolateyInstall.ps1
+++ src/tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'luaforwindows' 'exe' '/VERYSILENT' 'http://luaforwindows.googlecode.com/files/LuaForWindows_v5.1.4-46.exe'
+Install-ChocolateyPackage 'lua' 'exe' '/VERYSILENT' 'http://luaforwindows.googlecode.com/files/LuaForWindows_v5.1.4-46.exe'
